8db1b00a2aa8ab6b11f46d2ca29c6936e01e8a1f
SifuUA <alekseykres@gmail.com>
2017-07-11 10:34:49 +0300

    added filters for different getting books from DB

--- src/main/java/com/okres/library/dao/impl/BookDaoImpl.java
+++ src/main/java/com/okres/library/dao/impl/BookDaoImpl.java
@@ -6,10 +6,7 @@ import com.okres.library.entities.Book;
 import com.okres.library.entities.Genre;
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.ProjectionList;
-import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.*;
 import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -48,28 +45,29 @@ public class BookDaoImpl implements BookDao {
 
     @Transactional
     public List<Book> getBooks() {
-        DetachedCriteria bookListCreteria = DetachedCriteria.forClass(Book.class, "b");
-        creatAliases(bookListCreteria);
+       List<Book> books = createBookList(createBookCriteria());
+       return books;
+    }
 
-        List<Book> books = createBookList(bookListCreteria);
-
+    @Transactional
+    public List<Book> getBooks(Author author) {
+        List<Book> books = createBookList(createBookCriteria().add(Restrictions.ilike("author.fio",author.getFio(), MatchMode.ANYWHERE)));
         return books;
     }
 
-    public List<Book> getBooks(Author author) {
-        return null;
-    }
-
     public List<Book> getBooks(String bookName) {
-        return null;
+        List<Book> books = createBookList(createBookCriteria().add(Restrictions.ilike("b.name",bookName, MatchMode.ANYWHERE)));
+        return books;
     }
 
     public List<Book> getBooks(Genre genre) {
-        return null;
+        List<Book> books = createBookList(createBookCriteria().add(Restrictions.ilike("b.genre", genre.getName(), MatchMode.ANYWHERE)));
+        return books;
     }
 
     public List<Book> getBooks(Character letter) {
-        return null;
+        List<Book> books = createBookList(createBookCriteria().add(Restrictions.ilike("b.name", String.valueOf(letter), MatchMode.ANYWHERE)));
+        return books;
     }
 
 //    creat aliases
@@ -84,5 +82,11 @@ public class BookDaoImpl implements BookDao {
         Criteria criteria = bookListCriteria.getExecutableCriteria(sessionFactory.getCurrentSession());
         criteria.addOrder(Order.asc("b.name")).setProjection(bookProjection).setResultTransformer(Transformers.aliasToBean(Book.class));
         return criteria.list();
+    }
+
+    public DetachedCriteria createBookCriteria() {
+        DetachedCriteria bookListCreteria = DetachedCriteria.forClass(Book.class, "b");
+        creatAliases(bookListCreteria);
+        return bookListCreteria;
     }
 }
